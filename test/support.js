4706f2e49ada7eaa491cd830d2a5cd3e9b645d94
Miroslav Bajtos <miroslav@strongloop.com>
2013-11-19 10:43:01 +0100

    Use the configured error handler for unknown urls
    Reordered middleware setup in app.js and added urlNotFound handler.
    
    Started integration tests that boot the application and issue mock requests
    using supertest.

--- test/support.js
+++ test/support.js
@@ -1,6 +1,8 @@
 var assert = require('assert');
 var fs = require('fs');
+var path = require('path');
 var expect = require('chai').expect;
+var fstools = require('fs-tools');
 
 assertFileExists = function (file) {
   assert(fs.existsSync(file), file + ' does not exist');
@@ -11,3 +13,20 @@ assertJSONFileHas = function(file, propertyPath, val) {
   var obj = JSON.parse(contents);
   expect(obj).to.have.deep.property(propertyPath, val);
 }
+
+SANDBOX = path.resolve(__dirname, 'sandbox/');
+
+givenEmptySandbox = function(cb) {
+  fstools.remove(SANDBOX, cb);
+
+  // Remove any cached modules from SANDBOX
+  for (var key in require.cache) {
+    if (key.slice(0, SANDBOX.length) == SANDBOX)
+      delete require.cache[key];
+  }
+}
+
+// Let express know that we are runing from unit-tests
+// This way the default error handler does not log
+// errors to STDOUT
+process.env.NODE_ENV = 'test';
