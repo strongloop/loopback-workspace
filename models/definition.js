strongloop/loopback-workspace

commit 47837ffffb34297d0db3be9ef8e1e0959704dff1

Definition.addToCache: remove related models
When adding a definition to datasource cache, data of the related models
embedded in the configuration must be removed.

See strongloop/loopback-datasource-juggler#242

models/definition.js
@@ -1,6 +1,7 @@
 var loopback = require('loopback');
 var path = require('path');
 var app = require('../app');
+var clone = require('underscore').clone;
 var debug = require('debug')('workspace:definition');
 var ConfigFile = app.models.ConfigFile;
 
@@ -144,5 +145,13 @@ Definition.addRelatedToCache = function(cache, fileData, facetName, fk) {
   });
 }
 
-function noop() {};
+Definition.addToCache = function(cache, val) {
+  // Remove data of embedded relations
+  // see https://github.com/strongloop/loopback-datasource-juggler/issues/242
+  var data = clone(val);
+  this.getEmbededRelations().forEach(function(relation) {
+    delete data[relation.as];
+  });
+  Definition.base.addToCache.call(this, cache, data);
+};
 

commit 7ac8c6fc848ced22fd0e67f7f7dc9b5eeeeb9b0e

Definition: fix loadToCache to return `id`
Fix the bug introduced by 47837ff where `Definition.loadToCache`
no longer returned the id of the created instance. That caused
`Facet.loadIntoCache` to throw because facetId was undefined.

models/definition.js
@@ -152,6 +152,6 @@ Definition.addToCache = function(cache, val) {
   this.getEmbededRelations().forEach(function(relation) {
     delete data[relation.as];
   });
-  Definition.base.addToCache.call(this, cache, data);
+  return Definition.base.addToCache.call(this, cache, data);
 };
 