9a496520db83a31b356612a31d1fcc8f7fec131e
Ron Edgecomb <ron.edgecomb@gmail.com>
2015-02-26 17:18:10 -0500

    Disable legacy explorer routes by default. Add option to generated config file for the new legacyExplorer option introduced as a fix to strongloop/loopback#1134. This will ensure that the /routes and /models routes are disabled by default.

--- test/end-to-end.js
+++ test/end-to-end.js
@@ -153,6 +153,24 @@ describe('end-to-end', function() {
       });
       // the test will time out if `booted` is not emitted
     });
+
+    it('has legacy explorer disabled in config', function (done) {
+      expect(app.get('legacyExplorer'), 'legacyExplorer option').to.be.false();
+      done();
+    });
+
+    it('has legacy explorer route /models disabled', function (done) {
+      request(app)
+        .get('/api/models')
+        .expect(404, done);
+    });
+
+    it('has legacy explorer route /routes disabled', function (done) {
+      request(app)
+        .get('/api/routes')
+        .expect(404, done);
+    });
+
   });
 
   describe('autoupdate', function() {
