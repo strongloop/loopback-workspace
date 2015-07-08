1f1e779a7e239ab568cf5deb47f6af36d32f1159
Raymond Feng <raymond@strongloop.com>
2015-07-08 16:55:40 -0700

    Clean up built-in policy models

---
+++ models/metrics-policy.js
@@ -0,0 +1,5 @@
+var app = require('../app');
+var debug = require('debug')('workspace:policy:metrics');
+
+var Metrics = app.models.Metrics;
+
--- models/oauth2-policy.js
+++ models/oauth2-policy.js
@@ -1,8 +1,6 @@
 var app = require('../app');
-var loopback = require('loopback');
 var debug = require('debug')('workspace:policy:oauth2');
 
-var Middleware = app.models.Middleware;
 var OAuth2 = app.models.OAuth2;
 
 OAuth2.prototype.defineScope = function(scope, cb) {
