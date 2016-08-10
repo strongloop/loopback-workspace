c75228ada69c6d76523dd375644056861e0a4414
Simon Ho <simonho@ca.ibm.com>
2016-08-10 16:08:14 -0700

    Fix eslint setup in empty-server template
    - Add lint command to the scripts section in package.json
    - Change linting from pretest to posttest
    - Update ESLint dep from 2.5.3 to 2.13.1 in package.json
    - Add eslint-config-loopback as a dev dependency
    - Add "use strict" to various template files

--- templates/projects/empty-server/data.js
+++ templates/projects/empty-server/data.js
@@ -13,9 +13,9 @@ template.package = {
   'version': '1.0.0',
   'main': 'server/server.js',
   'scripts': {
+    'lint': 'eslint .',
     'start': 'node .',
-    'pretest': 'eslint .',
-    'posttest': 'nsp check',
+    'posttest': 'npm run lint && nsp check',
   },
   'dependencies': {
     'compression': '^1.0.3',
@@ -27,7 +27,8 @@ template.package = {
     'strong-error-handler': '^1.0.1',
   },
   'devDependencies': {
-    'eslint': '^2.5.3',
+    'eslint': '^2.13.1',
+    'eslint-config-loopback': '^4.0.0',
     'nsp': '^2.1.0',
   },
   // Avoid NPM warning
