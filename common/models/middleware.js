4acbd205309908cc71abb4f5d9132e648d88b8d3
deepakrkris <deepak.r.kris@gmail.com>
2017-01-08 15:59:53 -0800

    Add loopback model to read middleware
    Functionality to read middleware configuration from workspace

--- common/models/middleware.js
+++ common/models/middleware.js
@@ -16,6 +16,16 @@ module.exports = function(Middleware) {
     }
     return phase;
   };
+  Middleware.getPhaseFromId = function(id) {
+    const parts = id.split('.');
+    if (parts.length > 1) {
+      return parts[0] + (parts.length > 2 ? ':' + parts[1] : '');
+    }
+  };
+  Middleware.getMiddlewarePath = function(id) {
+    const parts = id.split('.');
+    return parts.length > 1 ? parts.pop() : id;
+  };
   Middleware.on('dataSourceAttached', function(eventData) {
     Middleware.create = function(data, options, cb) {
       if (typeof options === 'function') {
@@ -26,6 +36,16 @@ module.exports = function(Middleware) {
       const connector = Middleware.getConnector();
       // TODO(Deepak) - add response handling later
       connector.createMiddleware(phase, data, cb);
+    };
+    Middleware.find = function(id, options, cb) {
+      if (typeof options === 'function') {
+        cb = options;
+        options = null;
+      }
+      const phase = Middleware.getPhaseFromId(id);
+      const middlewarePath = Middleware.getMiddlewarePath(id);
+      const connector = Middleware.getConnector();
+      connector.findMiddleware(phase, middlewarePath, cb);
     };
   });
 };
