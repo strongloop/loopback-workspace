7c1c6169e73d91e6a65458432a5061857970e9ab
deepakrkris <deepak.r.kris@gmail.com>
2017-02-13 10:53:35 -0800

    Refactor templates test cases
    Call world features from test cases

--- test/acceptance/use-cases/test-scripts/create-from-templates.js
+++ test/acceptance/use-cases/test-scripts/create-from-templates.js
@@ -8,7 +8,6 @@ const path = require('path');
 const testSupport = require('../../../helpers/test-support');
 const util = require('util');
 const workspaceManager = require('../../../../component/workspace-manager');
-const TYPE_OF_TEST = 'acceptance';
 
 const Workspace = app.models.Workspace;
 app.on('booted', function() {
@@ -16,75 +15,60 @@ app.on('booted', function() {
 });
 
 module.exports = function() {
-  const testsuite = this;
   this.Given(/^that the templates are loaded$/, function(next) {
     app.on('templates-loaded', next);
   });
 
   this.When(/^I create a workspace from the template '(.+)'$/,
   function(templateName, next) {
-    testsuite.templateName = templateName;
-    testsuite.destinationPath =
-      testSupport.givenSandboxDir(TYPE_OF_TEST, testsuite.templateName);
-    testSupport.givenEmptySandbox(testsuite.destinationPath, function(err) {
-      if (err) return next(err);
-      const data = {
-        templateName: testsuite.templateName,
-        destinationPath: testsuite.destinationPath,
-      };
-      Workspace.create(data, {}, next);
-    });
+    this.createWorkspace(Workspace, templateName, next);
   });
 
   this.Then(/^the workspace is created$/, function(next) {
-    const workspace =
-      workspaceManager.getWorkspaceByFolder(testsuite.destinationPath);
-    const dir = workspace.getDirectory();
-    const template = workspaceManager.getTemplate(testsuite.templateName);
+    const testsuite = this;
+    this.setup(this.templateName);
+    const template = workspaceManager.getTemplate(this.templateName);
     const tasks = [];
     if (template.server) {
-      const serverDir = path.join(dir, 'server');
-      tasks.push(function(cb) {
-        fs.readdir(serverDir, function(err, fileList) {
-          if (err) return cb(err);
-          const expectedList = ['config.json', 'model-config.json'];
-          expect(fileList).to.include.members(expectedList);
-          cb();
-        });
-      });
-      if (template.server.modelConfigs) {
-        tasks.push(function(cb) {
-          const facet = workspace.getFacet('server');
-          const modelConfigPath = facet.getModelConfigPath();
-          fs.readJson(modelConfigPath, function(err, modelConfigs) {
-            if (err) return cb(err);
-            const expectedList = [];
-            template.server.modelConfigs.forEach(function(modelConfig) {
-              if (modelConfig.name)
-                expectedList.push(modelConfig.name);
-            });
-            delete modelConfigs._meta;
-            expect(Object.keys(modelConfigs).length).to
-              .eql(expectedList.length);
-            cb();
-          });
-        });
-      }
-      if (template.server.datasources) {
-        tasks.push(function(cb) {
-          const configFile = workspace.getDataSourceConfigFilePath();
-          fs.readJson(configFile, function(err, datasources) {
-            if (err) return cb(err);
-            const expectedList = [];
-            template.server.datasources.forEach(function(ds) {
-              expectedList.push(ds.name);
-            });
-            expect(Object.keys(datasources)).to.include.members(expectedList);
-            cb();
-          });
-        });
-      }
+      addTaskToCheckFacet(tasks, testsuite);
+    }
+    if (template.server.modelConfigs) {
+      addTaskToCheckModelConfig(tasks, testsuite, template);
+    };
+    if (template.server.datasources) {
+      addTaskToCheckDataSource(tasks, testsuite, template);
     }
     async.series(tasks, next);
+  });
+};
+
+function addTaskToCheckFacet(tasks, testsuite) {
+  tasks.push(function(cb) {
+    testsuite.checkFacet(cb);
+  });
+}
+
+function addTaskToCheckModelConfig(tasks, testsuite, template) {
+  tasks.push(function(cb) {
+    testsuite.readModelConfig(function(err, modelConfigs) {
+      delete modelConfigs._meta;
+      const expectedLength = template.server.modelConfigs.length;
+      expect(Object.keys(modelConfigs).length).to.eql(expectedLength);
+      cb();
+    });
+  });
+}
+
+function addTaskToCheckDataSource(tasks, testsuite, template) {
+  const expectedDataSources = [];
+  template.server.datasources.forEach(function(ds) {
+    expectedDataSources.push(ds.name);
+  });
+  tasks.push(function(cb) {
+    testsuite.readDataSource(function(err, datasources) {
+      if (err) return cb(err);
+      expect(Object.keys(datasources)).to.include.members(expectedDataSources);
+      cb();
+    });
   });
 };
