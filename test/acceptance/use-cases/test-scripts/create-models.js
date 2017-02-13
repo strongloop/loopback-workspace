f9df5b730c558e22b3bfb63bc97d3b20964184c7
deepakrkris <deepak.r.kris@gmail.com>
2017-02-13 10:59:03 -0800

    Refactor model test cases
    Refactor test cases as per functionality

--- test/acceptance/use-cases/test-scripts/create-models.js
+++ test/acceptance/use-cases/test-scripts/create-models.js
@@ -8,7 +8,6 @@ const path = require('path');
 const testSupport = require('../../../helpers/test-support');
 const util = require('util');
 const workspaceManager = require('../../../../component/workspace-manager');
-const TYPE_OF_TEST = 'acceptance';
 
 const ModelDefinition = app.models.ModelDefinition;
 const ModelMethod = app.models.ModelMethod;
@@ -24,7 +23,7 @@ module.exports = function() {
   this.Given(/^that I have loaded the workspace '(.+)'$/,
   function(templateName, next) {
     testsuite.workspaceDir =
-      testSupport.givenSandboxDir(TYPE_OF_TEST, templateName);
+      testSupport.givenSandboxDir(templateName);
     testsuite.workspace =
       workspaceManager.getWorkspaceByFolder(testsuite.workspaceDir);
     testsuite.workspaceId = testsuite.workspace.getId();
@@ -67,135 +66,12 @@ module.exports = function() {
   this.Given(/^the model '(.+)' exists in workspace '(.+)'$/,
   function(modelName, workspaceName, next) {
     testsuite.modelId = 'common.models.' + modelName;
-    const dir = testSupport.givenSandboxDir(TYPE_OF_TEST, workspaceName);
+    const dir = testSupport.givenSandboxDir(workspaceName);
     testsuite.workspace = workspaceManager.getWorkspaceByFolder(dir);
     testsuite.workspaceId = testsuite.workspace.getId();
     const storedModel = testsuite.workspace.getModel(testsuite.modelId);
     expect(storedModel).to.not.to.be.undefined();
     expect(storedModel).to.be.an.instanceOf(ModelClass);
     next();
-  });
-
-  this.When(/^I add property '(.+)' of type '(.+)'$/,
-    function(propertyName, type, next) {
-      testsuite.propertyId = propertyName;
-      const propertyDef = {
-        modelId: testsuite.modelId,
-        name: propertyName,
-        type: type,
-      };
-      const options = {workspaceId: testsuite.workspaceId};
-      ModelProperty.create(propertyDef, options, function(err, data) {
-        if (err) return next(err);
-        testsuite.expectedProperty = propertyDef;
-        next();
-      });
-    });
-
-  this.Then(/^the model property is created$/, function(next) {
-    const model = testsuite.workspace.getModel(testsuite.modelId);
-    const file = model.getFilePath();
-    fs.readJson(file, function(err, data) {
-      if (err) return next(err);
-      const property = data &&
-        data.properties &&
-        data.properties[testsuite.expectedProperty.name];
-      expect(property).to.not.to.be.undefined();
-      expect(testsuite.expectedProperty).to.eql(property);
-      next();
-    });
-  });
-
-  this.Given(/^I add relation '(.+)' from '(.+)' to '(.+)'$/,
-  function(relationName, fromModelName, toModelName, next) {
-    testsuite.fromModelName = fromModelName;
-    testsuite.toModelName = toModelName;
-    testsuite.relationName = relationName;
-    next();
-  });
-
-  this.When(/^the relation is of type '(.+)' and foreignKey '(.+)'$/,
-    function(relationType, foreignKey, next) {
-      const relationDef = {
-        id: testsuite.relationName,
-        type: relationType,
-        foreignKey: foreignKey,
-        modelId: testsuite.fromModelName,
-        model: testsuite.toModelName,
-        facetName: 'common.models',
-      };
-      const options = {workspaceId: testsuite.workspaceId};
-      testsuite.expectedRelation = relationDef;
-      ModelRelation.create(relationDef, options, function(err) {
-        if (err) return next(err);
-        next();
-      });
-    });
-
-  this.Then(/^the model relation is created$/, function(next) {
-    const relationDef = testsuite.expectedRelation;
-    const facetName = relationDef.facetName;
-    const fromModelName = relationDef.modelId;
-    delete relationDef.id;
-    delete relationDef.facetName;
-    delete relationDef.modelId;
-    const model =
-      testsuite.workspace.getModel(facetName + '.' + fromModelName);
-    const file = model.getFilePath();
-    fs.readJson(file, function(err, data) {
-      if (err) return next(err);
-      const relation = data &&
-        data.relations &&
-        data.relations[testsuite.relationName];
-      expect(relation).to.not.to.be.undefined();
-      expect(testsuite.expectedRelation).to.eql(relation);
-      next();
-    });
-  });
-
-  this.Given(/^I add model method '(.+)' in workspace '(.+)'$/,
-  function(methodName, workspaceName, next) {
-    testsuite.methodName = methodName;
-    testsuite.modelMethod = {accepts: [], returns: []};
-    next();
-  });
-
-  this.When(/^the method has an argument '(.+)' type '(.+)'$/,
-    function(name, type, next) {
-      const argument = {arg: name, type: type};
-      testsuite.modelMethod.accepts.push(argument);
-      next();
-    });
-
-  this.When(/^the method has a return parameter '(.+)' type '(.+)'$/,
-    function(name, type, next) {
-      const param = {arg: name, type: type};
-      testsuite.modelMethod.returns.push(param);
-      next();
-    });
-
-  this.When(/^I call the model method api$/,
-    function(next) {
-      testsuite.modelMethod.name = testsuite.methodName;
-      testsuite.modelMethod.modelId = testsuite.modelId;
-      const options = {workspaceId: testsuite.workspaceId};
-      ModelMethod.create(testsuite.modelMethod, options, function(err) {
-        if (err) return next(err);
-        next();
-      });
-    });
-
-  this.Then(/^the model method is created$/, function(next) {
-    const model = testsuite.workspace.getModel(testsuite.modelId);
-    const file = model.getFilePath();
-    fs.readJson(file, function(err, data) {
-      if (err) return next(err);
-      const method = data &&
-        data.methods &&
-        data.methods[testsuite.methodName];
-      expect(method).to.not.to.be.undefined();
-      expect(testsuite.modelMethod).to.eql(method);
-      next();
-    });
   });
 };
