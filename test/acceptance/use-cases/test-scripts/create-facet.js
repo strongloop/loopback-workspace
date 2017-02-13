68759a8de9c079a38c1d7bde0862ca2417947b14
deepakrkris <deepak.r.kris@gmail.com>
2017-02-13 10:50:35 -0800

    Refactor Facet test cases
    Call test suite features from Facet tests

--- test/acceptance/use-cases/test-scripts/create-facet.js
+++ test/acceptance/use-cases/test-scripts/create-facet.js
@@ -7,7 +7,6 @@ const path = require('path');
 const testSupport = require('../../../helpers/test-support');
 const util = require('util');
 const workspaceManager = require('../../../../component/workspace-manager');
-const TYPE_OF_TEST = 'acceptance';
 
 const Facet = app.models.Facet;
 app.on('booted', function() {
@@ -18,42 +17,23 @@ module.exports = function() {
   const testsuite = this;
   this.Given(/^that I have loaded the workspace '(.+)'$/,
   function(workspaceName, next) {
-    const dir = testSupport.givenSandboxDir(TYPE_OF_TEST, workspaceName);
-    testsuite.workspace = workspaceManager.getWorkspaceByFolder(dir);
+    this.setup(workspaceName);
     next();
   });
 
   this.When(/^I create a facet '(.+)'$/,
   function(facetName, next) {
-    const modelsMeta = {
-      sources: [
-        'loopback/common/models',
-        'loopback/server/models',
-        '../common/models',
-        './models',
-      ],
-      mixins: [
-        'loopback/common/mixins',
-        'loopback/server/mixins',
-        '../common/mixins',
-        './mixins',
-      ],
-    };
     const config = {
       name: facetName,
-      modelsMetadata: modelsMeta,
+      modelsMetadata: this.getDefaultModelsMeta(),
     };
-    const options = {workspaceId: testsuite.workspace.getId()};
-    Facet.create(config, options, function(err, data) {
-      if (err) return next(err);
-      delete config.id;
-      testsuite.facetName = facetName;
-      next();
-    });
+    this.saveInputs(config);
+    this.createModel(Facet, next);
   });
 
   this.Then(/^the facet is created$/, function(next) {
-    const facet = testsuite.workspace.getFacet(testsuite.facetName);
+    const inputs = this.getSavedInputs();
+    const facet = this.workspace.getFacet(inputs.facetName);
     expect(facet).to.not.to.be.undefined();
     const dir = facet.getPath();
     fs.exists(dir, function(isExists) {
