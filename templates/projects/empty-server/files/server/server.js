29a857d373b3ee22618fb520861cdd6888725dcd
Hage Yaapa <captain@hacksparrow.com>
2017-03-12 12:58:56 +0530

    remove whitespace

--- templates/projects/empty-server/files/server/server.js
+++ templates/projects/empty-server/files/server/server.js
@@ -21,7 +21,6 @@ app.start = function() {
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
-
   // start the server if `$ node server.js`
   if (require.main === module) {
     var server = app.start();
