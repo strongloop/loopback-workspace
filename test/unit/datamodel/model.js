538c9a386b8997bffaab0a685afccd30af2f45f5
deepakrkris <deepak.r.kris@gmail.com>
2017-03-21 14:00:11 -0700

    Add create action to model
    mixin ModelAction class with Model

--- test/unit/datamodel/model.js
+++ test/unit/datamodel/model.js
@@ -6,7 +6,10 @@
 
 const Model = require('../../../lib/datamodel/model');
 const expect = require('../../helpers/expect');
+const fs = require('fs-extra');
 const Workspace = require('../../../lib/workspace');
+const WorkspaceManager = require('../../../lib/workspace-manager');
+const testSupport = require('../../helpers/test-support');
 
 describe('Graph : Models', function() {
   let workspace;
@@ -14,13 +17,31 @@ describe('Graph : Models', function() {
 
   describe('constructor', function() {
     it('adds a new Model node to the graph', function() {
+      workspace = new Workspace('/');
+      workspace.addDomain('ModelDefinition');
       const model = new Model(workspace, 'test', {}, {});
       expect(workspace.getNode('ModelDefinition', 'test')).to.eql(model);
     });
   });
 
-  function createWorkspace() {
-    workspace = new Workspace('/');
-    workspace.addDomain('ModelDefinition');
+  describe('create()', function() {
+    it('write model definition file in workspace', function(next) {
+      workspace = WorkspaceManager.getWorkspace();
+      const data = {name: 'test', facetName: 'common'};
+      const model =
+        new Model(workspace, 'common.models.test', data);
+      model.create(function(err) {
+        const file = model.getFilePath();
+        fs.readJson(file, function(err, data) {
+          if (err) return next(err);
+          expect(Object.keys(data)).to.include.members(['facetName', 'name']);
+          next();
+        });
+      });
+    });
+  });
+
+  function createWorkspace(done) {
+    testSupport.givenBasicWorkspace('empty-server', done);
   }
 });
